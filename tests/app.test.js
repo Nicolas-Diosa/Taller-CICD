e8eca3bb51e688ec30119e539a545e9ff2363fe4
Juan Camargo <jucamargop@unal.edu.co>
2026-02-12 11:30:13 -0500

    add test for /version endpoint

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../src/app');
+const { version } = require('./package.json');
 
 describe('API Endpoints', () => {
     test('GET / should return welcome message', async() => {
@@ -12,5 +13,11 @@ describe('API Endpoints', () => {
         const respond = await request(app).get('/health');
         expect(respond.status).toBe(200);
         expect(respond.body.status).toBe('OK');
+    });
+
+    test('GET /version should return version', async() => {
+        const respond = await request(app).get('/version');
+        expect(respond.status).toBe(200);
+        expect(respond.version).toBe(version);
     });
 });
